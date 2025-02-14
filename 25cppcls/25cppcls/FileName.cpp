e59e1573e3c14a6274d4d79dca651772a374c49e
guswn3717 <149389998+guswn3717@users.noreply.github.com>
2025-02-14 19:44:03 +0900

    20250214

--- 25cppcls/25cppcls/FileName.cpp
+++ 25cppcls/25cppcls/FileName.cpp
@@ -52,7 +52,54 @@ public:
 			head = newNode;
 
 			newNode->data = data;
-			newNode->Next = nullptr;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			Node* curentNode = head;
+
+			while (curentNode->next != nullptr)
+			{
+				curentNode = curentNode->next;
+			}
+
+			curentNode->next = newNode;
+
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+
+		size++;
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+			return;
+		}
+		else
+		{
+			Node* deleteNode = head;
+			      
+			head = deleteNode->next;
+
+			delete deleteNode;
+
+			size--;
+		}
+	}
+
+	void show()
+	{
+		Node* currentNode = head;
+
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << " ";
+
+			currentNode = currentNode->next;
 		}
 	}
 };
@@ -63,6 +110,17 @@ int main()
 
 	singleLinkedList.push_front(10);
 	singleLinkedList.push_front(20);
+
+	singleLinkedList.push_back(5);
+	singleLinkedList.push_back(10);
+
+	//singleLinkedList.pop_front();
+	//singleLinkedList.pop_front();
+	//singleLinkedList.pop_front();
+	//singleLinkedList.pop_front();
+	//singleLinkedList.pop_front();
+
+	singleLinkedList.show();
 
 	return 0;
 }
