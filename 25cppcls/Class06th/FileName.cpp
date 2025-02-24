ada30bbf52ec4a93fde43c175b4e9b74e346bfda
guswn3717 <149389998+guswn3717@users.noreply.github.com>
2025-02-24 19:41:53 +0900

    20250224

---
+++ 25cppcls/Class06th/FileName.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#define SIZE 4
+
+using namespace std;
+
+template <typename T>
+class CircleQueue
+{
+private:
+	int rear;
+	int front;
+
+	T container[SIZE];
+public:
+	CircleQueue()
+	{
+		rear = SIZE - 1;
+		front = SIZE - 1;
+
+		//for(int )
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
