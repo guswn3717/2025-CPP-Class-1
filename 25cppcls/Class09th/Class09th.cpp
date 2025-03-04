bfade9e2a1f3fc2d7555a9b28a866329d5ea819a
guswn3717 <rudals9973@gmail.com>
2025-03-04 19:44:02 +0900

    20250304

---
+++ 25cppcls/Class09th/Class09th.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+#define SIZE 6
+
+using namespace std;
+
+template<typename KEY, typename VALUE>
+class HashTable
+{
+private:
+    struct Node
+    {
+        KEY key;
+        VALUE value;
+
+        Node* next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
+
+    Bucket bucket[SIZE];
+public:
+    HashTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            bucket[i].count = 0;
+            bucket[i].head = nullptr;
+        }
+    }
+
+    template <typename T>
+    const int& HashFunction(T key)
+    {
+        unsigned int hashIndex = (int)key % SIZE;
+
+        return hashIndex;
+    }
+
+    template<>
+    const int& HashFunction(const char* key)
+    {
+        int hash = 0;
+
+        for (int i = 0; i < strlen(key); i++)
+        {
+            hash += key[i];
+        }
+
+        int hashIndex = hash % SIZE;
+
+        return hashIndex;
+    }
+
+    Node* CreateNode(KEY key, VALUE value)
+    {
+        Node* newNode = new Node;
+
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        return newNode;
+    }
+
+    void Insert(KEY key, VALUE value)
+    {
+        // 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = HashFunction(key);
+
+        // 새로운 노드를 생성합니다.
+        Node* newNode = CreateNode(key, value);
+
+        //노드가 한개라도 존재하지않는다면
+        if (bucket[hashIndex].count == 0)
+        {
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        bucket[hashIndex].count++;
+    }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            Node* deleteNode = bucket[i].head;
+            Node* nextNode = bucket[i].head;
+
+            if (bucket[i].head == nullptr)
+            {
+                continue;
+            }
+            else
+            {
+                while (nextNode != nullptr)
+                {
+                    nextNode = deleteNode->next;
+
+                    delete deleteNode;
+
+                    deleteNode = nextNode;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    HashTable<const char*, int> hashTable;
+
+    hashTable.Insert("Sword", 10000);
+    hashTable.Insert("Armor", 5000);
+
+    return 0;
+}
